4b99fd3fba2de4575f688434275df2886668ff98
koreanstudent <hn12344@naver.com>
2020-01-18 23:40:03 +0900

    next와 express 연결

--- front/server.js
+++ front/server.js
@@ -1,3 +1,41 @@
 const express =require('express');
 const next = require('next');
 const morgan =require('morgan');
+const cookieParser =require('cookie-parser');
+const expressSession =require('express-session');
+const dotenv = require('dotenv');
+
+// next 서버와 express 연결
+const dev =process.env.NODE_ENV !== 'production';
+const prod =process.env.NODE_ENV === 'production';
+
+const app =next({ dev });
+const handle = app.getRequestHandler();
+dotenv.config();
+
+app.prepare().then( () => {
+    const server = express();
+
+    server.use(morgan('dev'));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true})); //req.body 작동
+    server.use(cookieParser(process.env.COOKIE_SECRET));
+    server.use(expressSession({
+        resave: false, // 매번 새션 강제 저장
+        saveUninitialized: false, // 빈 값도 저장
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true, // 자바스크립트로 쿠키에 접근 못함
+            secure: false, // https를 쓸 때 true
+        },
+        name: 'reactnode', // connent.id 쿠키 아이디 변경 보안을위해 변경해야함
+    }));
+    // * = 모든 요청을 처리하겠다.
+    server.get('*', (req, res) => {
+        return handle(req,res);
+    })
+
+    server.listen(3060, () => {
+        console.log('next+express running on port 3060');
+    })
+});
